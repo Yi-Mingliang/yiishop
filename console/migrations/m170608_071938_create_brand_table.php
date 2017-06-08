55632881ecc375784c6551f38a94bf9c1aff5086
Yi-Mingliang <776550514@qq.com>
2017-06-08 15:44:01 +0800

    项目初始版

---
+++ console/migrations/m170608_071938_create_brand_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `brand`.
+ */
+class m170608_071938_create_brand_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('brand', [
+            'id' => $this->primaryKey(),
+//name varchar﴿05﴾ 名称
+            'name'=>$this->string(50)->notNull()->comment('名称'),
+//intro text 简介
+            'intro'=>$this->text()->notNull()->comment('简介'),
+//logo varchar﴿552﴾ LOGO图片
+            'logo'=>$this->string(200)->comment('LOGO'),
+//sort int﴿11﴾ 排序
+            'sort'=>$this->integer(11)->comment('排序'),
+//status int﴿2﴾ 状态﴿常正1 藏隐0 除删1‐﴾
+            'status'=>$this->smallInteger(2)->comment('状态'),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('brand');
+    }
+}
